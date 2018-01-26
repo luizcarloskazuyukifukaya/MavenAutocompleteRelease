05bb291b4de170211cd62ae97bbcbd38893f7888
Luiz Carlos Kazuyuki Fukaya <ilovedigitalmeister@gmail.com>
2018-01-27 00:07:47 +0900

    XML Cache implemented as a prototype.

--- src/main/java/me/ilovedigitalmeister/AutoCompleteServlet.java
+++ src/main/java/me/ilovedigitalmeister/AutoCompleteServlet.java
@@ -41,11 +41,13 @@ public class AutoCompleteServlet extends HttpServlet {
     private static final int MAX_DISP_CANDIDATE_NUM_ITEM = 10; // Max display item number
     private static final int MAX_DISP_CACHE_NUM_ITEM = 100; // Max display item number
     private static final String FILE_EXTENSTION_NAME = ".xml";
+    private static final String BUCKET_UNIQUE_NAME = "autcomplete_xml_cache";
     
     //private static final int MAX_DISP_CACHE_NUM_ITEM = 3000; // Max display item number [FINAL]
     private static boolean debug = false;
     
     private static final Logger logger = Logger.getLogger(AutoCompleteServlet.class.getName());
+    final String bucketName = BUCKET_UNIQUE_NAME; // Bucket name
 
     // Cache for this Servlet
     // This cache will be updated regulary (Eg. every 24 hours);
@@ -142,7 +144,7 @@ public class AutoCompleteServlet extends HttpServlet {
     
     private void doAutoCompleteGet(String key, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        boolean namesAdded = false;
+        boolean addToXMLCache = false;
 
         StringBuilder sb = null;
                 
@@ -174,7 +176,7 @@ public class AutoCompleteServlet extends HttpServlet {
                             sb.append("<id>").append(product.getId()).append("</id>");
                             sb.append("<name>").append(product.getName()).append("</name>");
                             sb.append("</product>");
-                            namesAdded = true;
+                            addToXMLCache = true;
                             i++;
                         }                    
                         /**
@@ -196,19 +198,22 @@ public class AutoCompleteServlet extends HttpServlet {
                 logger.log(Level.INFO, " {0} entries actualy match with the keyword.",i);
             } else {
                 if(sb.length()>0) { //If the XML corresponding to the keyword was already stored in the storage
-                    namesAdded= true;
                     logger.log(Level.INFO, "Cache stored in the storage found for reuse. {0}", sb.toString());
+                } else {
+                    logger.log(Level.INFO, "Cache stored in the storage found but no entry exist. {0}", sb.toString());
+                    sb = null;
                 }
             }
         }
-        if (namesAdded && (sb != null)) {
+        if (sb != null) {
             response.setContentType("text/xml");
             response.setHeader("Cache-Control", "no-cache");
             response.getWriter().write("<products>" + sb.toString() + "</products>");
             
-            // Save into the Storage Cache so next time it can be used without recreating the data
-            putIntoStorageCache(key, sb.toString());
-
+            if(addToXMLCache) {
+                // Save into the Storage Cache so next time it can be used without recreating the data
+                putIntoStorageCache(key, sb.toString());                
+            }
         } else {
             //nothing to show
             logger.log(Level.INFO, "Could not find any entry for the given word.",products.size());
@@ -336,7 +341,7 @@ public class AutoCompleteServlet extends HttpServlet {
     }
 
     private StringBuilder getFromStorageCache(String key) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = null;
         /**
          * Here we should consider searching on the storage cache XML data created before.
          * Simply we should create a storage cache with the given word (String key) as KEY to retrieve the XML
@@ -348,10 +353,10 @@ public class AutoCompleteServlet extends HttpServlet {
          *   <name>HERE COMES THE PRODUCT NAME</name>
          *  <product>
          */
-        // NOT YET USING THE STORAGE TO CACHE THE XML FILES, HENCE, RETURN NULL FOR NOW.
         
         // _storageXMLCacheIndex.put(key, key+FILE_EXTENSTION_NAME);
-        String xmlFileName = _storageXMLCacheIndex.get(key);
+        final String xmlFileName = _storageXMLCacheIndex.get(key);
+        final String blobName = key+FILE_EXTENSTION_NAME;
 
         // no cache registered on Storage Cache
         if(xmlFileName == null) {
@@ -365,24 +370,28 @@ public class AutoCompleteServlet extends HttpServlet {
          * REFERENCE: https://github.com/GoogleCloudPlatform/google-cloud-java/tree/master/google-cloud-storage
          */
         Storage storage = StorageOptions.getDefaultInstance().getService();
-
-        // Create a bucket
-        String bucketName = "autcomplete_cache"; // Bucket name
         
-        // Retrieve a blob from the bucket        
-        BlobId blobId = BlobId.of(bucketName, key+FILE_EXTENSTION_NAME);
-        byte[] content = storage.readAllBytes(blobId);
-        String contentString = new String(content, UTF_8);
-
-        if(contentString.length() > 0) {
-            logger.log(Level.INFO, "Blob {0} includes product information data.", key+FILE_EXTENSTION_NAME);
-        } else {
-            logger.log(Level.INFO, "Blob {0} DOES NOT include product information data.", key+FILE_EXTENSTION_NAME);
-        }
-        sb.append(contentString);
-        
-        logger.log(Level.INFO, "XML content: {}", sb.toString());                    
-        
+        try {
+            // Retrieve a blob from the bucket        
+            BlobId blobId = BlobId.of(bucketName, blobName);
+            Blob blob = storage.get(blobId);
+            if(blob != null) {
+                byte[] content = storage.readAllBytes(blobId);
+                String contentString = new String(content, UTF_8);                
+                if(contentString.length() > 0) {
+                    logger.log(Level.INFO, "Blob {0} includes product information data.", blobName);
+                    sb = new StringBuilder();
+                    sb.append(contentString);
+                    logger.log(Level.INFO, "XML content: {0}", sb.toString());
+                } else {
+                    logger.log(Level.INFO, "Blob {0} DOES NOT include product information data.", blobName);
+                }
+            } else {
+                logger.log(Level.INFO, "{0} not found.", blobName);
+            }
+        } catch(com.google.cloud.storage.StorageException e) {
+            logger.log(Level.SEVERE, "{0}", e.toString());                        
+        }        
         return sb;
     }
 
@@ -397,6 +406,7 @@ public class AutoCompleteServlet extends HttpServlet {
          * Here store the xml file for future reference without creating the product name information associated with the key
          */
         logger.log(Level.INFO, "key:{0}", key);
+        final String blobName = key+FILE_EXTENSTION_NAME;
         
         if(key == null) {
             // this should never happen
@@ -408,10 +418,12 @@ public class AutoCompleteServlet extends HttpServlet {
 
                 // Save xml data into Google Storage
                 if(xmlStr != null) {
-                    putIntoStorageCache(key, xmlStr);
-                    // Put entry on Storage Cache manager
-                    // KEY: key, VALUE: filename ([key].xml
-                    _storageXMLCacheIndex.put(key, key+FILE_EXTENSTION_NAME);
+                    if(putIntoStorageCache(key, xmlStr)) {
+                        // Put entry on Storage Cache manager
+                        // KEY: key, VALUE: filename ([key].xml
+                        _storageXMLCacheIndex.put(key, blobName);                        
+                        logger.log(Level.INFO,"XML {0} cache registered.", blobName);                
+                    }
                 } else {
                     logger.log(Level.INFO," Entries matching {0} could not be found. Thus no XML cache was created in the Storage Cache.", key);                
                 }
@@ -422,7 +434,12 @@ public class AutoCompleteServlet extends HttpServlet {
         }
     }
 
-    private void putIntoStorageCache(String key, String cache) {
+    private boolean putIntoStorageCache(String key, String cache) {
+        boolean retVal = false;
+        if(key == null) return false;
+
+        final String blobName = key+FILE_EXTENSTION_NAME;
+
         /**
          * Here store the xml file for future reference without creating the product name information associated with the key
          */
@@ -434,30 +451,37 @@ public class AutoCompleteServlet extends HttpServlet {
          */
         Storage storage = StorageOptions.getDefaultInstance().getService();
 
-        final String bucketName = "autcomplete_cache"; // Bucket name
-        final String blobName = key+FILE_EXTENSTION_NAME;
+        logger.log(Level.INFO, "Target Bucket:{0}", bucketName);
+        logger.log(Level.INFO, "Target blob:{0}", blobName);
 
-        // Create a bucket
-        if(storage.get(bucketName, Storage.BucketGetOption.fields()) != null) {
-            storage.create(BucketInfo.of(bucketName));            
-            logger.log(Level.INFO, "Bucket {0} was created.", bucketName);
+        try {
+            // Create a bucket
+            if(storage.get(bucketName, Storage.BucketGetOption.fields()) == null) { //Does not exist
+                storage.create(BucketInfo.of(bucketName));  // create new bucket
+                logger.log(Level.INFO, "Bucket {0} was created.", bucketName);
+            }
+
+            // Upload a blob to the newly created bucket
+            BlobId blobId = BlobId.of(bucketName, blobName);
+            Blob blob = storage.get(blobId);
+            if(blob != null) { //blog found, let us delete it.
+                boolean deleted = blob.delete(BlobSourceOption.generationMatch());
+                if(deleted) { //existed and deleted
+                    logger.log(Level.INFO, "Blob {0} was found and deleted.", blobName);
+                } else {
+                    logger.log(Level.INFO, "Blob {0} was NOT found.", blobName);            
+                }            
+            }
+            // create new one
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
+            storage.create(blobInfo, cache.getBytes(UTF_8));            
+            logger.log(Level.INFO, "Blob {0} was created.", blobName);
+
+            retVal = true;
+        } catch(com.google.cloud.storage.StorageException e) {
+            logger.log(Level.SEVERE, "{0}", e.toString());                        
         }
-        
-        // Upload a blob to the newly created bucket
-        BlobId blobId = BlobId.of(bucketName, blobName);
-        Blob blob = storage.get(blobId);
-        if(blob != null) { //blog found, let us delete it.
-            boolean deleted = blob.delete(BlobSourceOption.generationMatch());
-            if(deleted) { //existed and deleted
-                logger.log(Level.INFO, "Blob {0} was found and deleted.", blobName);
-            } else {
-                logger.log(Level.INFO, "Blob {0} was NOT found.", blobName);            
-            }            
-        }
-        // create new one
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
-        storage.create(blobInfo, key.getBytes(UTF_8));            
-        logger.log(Level.INFO, "Blob {0} was created.", blobName);            
+        return retVal;
     }    
 }
 
